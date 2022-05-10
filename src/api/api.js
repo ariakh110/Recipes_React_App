ba313fb6df3c9d01b84545ec0ca6e9a398b2d952
aria khayer <ariakh110@gmail.com>
2022-05-10 12:47:08 +0430

    refine .gitignore

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 
+const key = process.env.REACT_APP_API_KEY;
 const server = axios.create({
-  baseURL: "GET https://api.spoonacular.com/recipes/random",
+  baseURL: `https://api.spoonacular.com/recipes/random?apiKey=${key}`,
 });
+
+const getRandomRecipe = async () => {
+  const data = await await fetch(
+    `httpa://api.spoonacular.com/recipes/random?apiKey=${key}`
+  ).json;
+  console.log(data);
+
+  //const { data } = await server.get();
+  return data;
+};
+
+export { getRandomRecipe };
--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,11 @@
+import React, { useEffect } from "react";
+import * as api from "../api/api";
+
 const Popular = () => {
+  useEffect(() => {
+    const data = api.getRandomRecipe();
+  }, []);
+
   return <div>Popular</div>;
 };
 
